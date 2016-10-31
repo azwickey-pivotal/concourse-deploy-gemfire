c1859fd1a07c28dc52d8aab076b2875eba6308cb
azwickey-pivotal <vania12R>
2016-10-31 13:55:44 -0400

    update validation

--- ci/tasks/validate-multi-az-redundancy-map.sh
+++ ci/tasks/validate-multi-az-redundancy-map.sh
@@ -3,7 +3,7 @@ ls -lha
 wget -O "pivotal-gemfire.deb" --post-data="" --header="Authorization: Token ${PIVNET_TOKEN}" https://network.pivotal.io/api/v2/products/pivotal-gemfire/releases/1753/product_files/4635/download
 sudo dpkg -i pivotal-gemfire.deb
 sudo apt-get update
-sudo apt-get -y install default-jre-headless 
+sudo apt-get -y install default-jre-headless
 gfsh version
 gfsh \
 -e "connect --locator=${LOCATOR_CONNECTION}" \
@@ -20,14 +20,9 @@ gfsh \
 -e "describe config --member=cacheserver-0" \
 -e "describe config --member=cacheserver-1" \
 -e "describe config --member=cacheserver-2" \
--e "describe config --member=cacheserver-3" \
--e "describe config --member=cacheserver-4" \
--e "describe config --member=cacheserver-5" \
 -e "show metrics --categories=partition --region=testing" > gemfire-output.txt
 cat gemfire-output.txt
 # checks redundancy in regions using the suggested method from pivotal docs:
 # http://gemfire.docs.pivotal.io/docs-gemfire/latest/developing/partitioned_regions/checking_region_redundancy.html
 cat gemfire-output.txt | grep numBucketsWithoutRedundancy | grep "| 0" &&
-cat gemfire-output.txt | grep redundancy-zone | grep ": AZ1" &&
-cat gemfire-output.txt | grep redundancy-zone | grep ": AZ2" &&
-cat gemfire-output.txt | grep redundancy-zone | grep ": AZ3"
+cat gemfire-output.txt | grep redundancy-zone | grep ": us-east1-b"
